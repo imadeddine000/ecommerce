e8942d3d77ba63511e406116252bdec96b0796c5
imadeddinekebour <imadeddinekebourlite@gmail.com>
2023-09-18 18:14:27 +0100

    f

--- server/main.js
+++ server/main.js
@@ -90,11 +90,11 @@ app.get('/get-commands',async (req,res)=>{
 
 })
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('*', (req, res) => {
   
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 app.listen(3001||process.env.PORT,()=>{
